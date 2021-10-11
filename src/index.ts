d4b36dad045f16863a4f90f1d9d373d44d32963c
Jay Wolfe <wolfejw86@gmail.com>
2021-10-10 21:39:55 -0400

    config interface

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,19 @@ import debug from 'debug';
 const log = debug('sourced-repo-dynamodb');
 
 interface TEntityType<TEntity extends Entity> {
-    new (snapshot?: any, events?: any[]): TEntity;
+  new (snapshot?: any, events?: any[]): TEntity;
 }
 
 interface RepositoryOptions {
-    snapshotFrequency: number;
+  dynamoTable: string;
+  snapshotFrequency: number;
+  dynamoIndex?: string;
+}
+
+interface RepositoryInitOptions {
+  dynamoTable: string;
+  snapshotFrequency?: number;
+  dynamoIndex?: string;
 }
 
 /**
@@ -20,145 +28,150 @@ interface RepositoryOptions {
  */
 
 export class Repository<TEntity extends Entity & { id: string }> {
-    private snapshotFrequency: number;
-    private snapshotPrefix: string;
-    private eventPrefix: string;
+  private snapshotFrequency: number;
+  private snapshotPrefix: string;
+  private eventPrefix: string;
+
+  /**
+   * TODO - figure out if it makes sense to keep these as any or to type
+   * as reference objects to ddb actions
+   */
+  private events: any;
+  private snapshots: any;
+  private options: RepositoryOptions;
+  private entityName: string;
+
+  constructor(
+    private entityType: TEntityType<TEntity>,
+    options: RepositoryInitOptions,
+  ) {
+    this.options = Object.assign(
+      {
+        snapshotFrequency: 10,
+      },
+      options,
+    );
+
+    this.snapshotFrequency = this.options.snapshotFrequency;
+    this.entityName = entityType.name.toLowerCase();
+    this.eventPrefix = `${this.entityName}events`;
+    this.snapshotPrefix = `${this.entityName}snapshots`;
+  }
+
+  init() {
+    log('init');
+  }
+
+  async get(id: string) {
+    // get latest snapshot by id
+    // get latest events after latest snapshot by entity's id
+
+    log(id);
+
+    // merge snapshot and events with new entity
+    return new this.entityType();
+  }
+
+  async getAll(ids: string[]) {
+    // get latest snapshots for each id
+    // get latest events after timestamp of latest snapshot for each entity's respective id
+
+    log(ids);
+
+    // merge snapshot and events with new entity
+    return new this.entityType();
+  }
+
+  async commit(
+    entity: TEntity,
+    options: { forceSnapshot: boolean; withTransaction?: boolean } = {
+      forceSnapshot: false,
+      withTransaction: true,
+    },
+  ) {
+    // write events
+    // write snapshot
+    // throw on error
+
+    log(options);
+
+    return;
+  }
+
+  async commitAll(
+    entities: TEntity[],
+    options: { forceSnapshots: boolean; withTransaction: boolean } = {
+      forceSnapshots: false,
+      withTransaction: true,
+    },
+  ) {
+    // write events for all entities
+    // write snapshots for all entities
+    // throw on error
+
+    log(options);
+
+    return;
+  }
+
+  private async commitEvents(entity: TEntity) {
+    if (entity.newEvents.length === 0) {
+      return;
+    }
+
+    if (!entity.id) {
+      new Error(
+        `Cannot commit an entity of type [${this.entityType.name}] without an [id] property`,
+      );
+    }
+
+    const events = entity.newEvents;
+
+    events.forEach((event: any) => {
+      event.id = entity.id;
+    });
+
+    log('Inserting events for %s', this.entityType.name);
 
     /**
-     * TODO - figure out if it makes sense to keep these as any or to type
-     * as reference objects to ddb actions
+     *  TODO - use batch put operations with retries OR a dynamodb transaction
      */
-    private events: any;
-    private snapshots: any;
+    // await Promise.all(events.map((event: any) => this.events.put(event)));
 
-    private entityName: string;
+    log('Successfully committed events for %s', this.entityType.name);
 
-    constructor(
-        private entityType: TEntityType<TEntity>,
-        private opts: RepositoryOptions = {
-            snapshotFrequency: 10,
-        },
+    entity.newEvents = [];
+  }
+
+  private async commitSnapshots(
+    entity: TEntity,
+    options = { forceSnapshot: false },
+  ) {
+    if (
+      options.forceSnapshot ||
+      entity.version >= entity.snapshotVersion + this.snapshotFrequency
     ) {
-        this.snapshotFrequency = this.opts.snapshotFrequency;
-        this.entityName = entityType.name.toLowerCase();
-        this.eventPrefix = `${this.entityName}events`;
-        this.snapshotPrefix = `${this.entityName}snapshots`;
+      const snapshot = entity.snapshot();
+
+      log('Inserting snapshot for %s', this.entityType.name);
+
+      /**
+       * TODO - map to new snapshot put dynamodb operation
+       */
+      await this.snapshots.put(snapshot);
+
+      log('Successfully committed snapshot for %s', this.entityType.name);
     }
+  }
 
-    init() {
-        log('init');
-    }
+  private emitEvents(entity: TEntity) {
+    const eventsToEmit = entity.eventsToEmit;
+    entity.eventsToEmit = [];
+    eventsToEmit.forEach((event) => {
+      const args = Array.from(event);
 
-    async get(id: string) {
-        // get latest snapshot by id
-        // get latest events after latest snapshot by entity's id
-
-        log(id);
-
-        // merge snapshot and events with new entity
-        return new this.entityType();
-    }
-
-    async getAll(ids: string[]) {
-        // get latest snapshots for each id
-        // get latest events after timestamp of latest snapshot for each entity's respective id
-
-        log(ids);
-
-        // merge snapshot and events with new entity
-        return new this.entityType();
-    }
-
-    async commit(
-        entity: TEntity,
-        options: { forceSnapshot: boolean; withTransaction?: boolean } = {
-            forceSnapshot: false,
-            withTransaction: true,
-        },
-    ) {
-        // write events
-        // write snapshot
-        // throw on error
-
-        log(options);
-
-        return;
-    }
-
-    async commitAll(
-        entities: TEntity[],
-        options: { forceSnapshots: boolean; withTransaction: boolean } = {
-            forceSnapshots: false,
-            withTransaction: true,
-        },
-    ) {
-        // write events for all entities
-        // write snapshots for all entities
-        // throw on error
-
-        log(options);
-
-        return;
-    }
-
-    private async commitEvents(entity: TEntity) {
-        if (entity.newEvents.length === 0) {
-            return;
-        }
-
-        if (!entity.id) {
-            new Error(
-                `Cannot commit an entity of type [${this.entityType.name}] without an [id] property`,
-            );
-        }
-
-        const events = entity.newEvents;
-
-        events.forEach((event: any) => {
-            event.id = entity.id;
-        });
-
-        log('Inserting events for %s', this.entityType.name);
-
-        /**
-         *  TODO - use batch put operations with retries OR a dynamodb transaction
-         */
-        // await Promise.all(events.map((event: any) => this.events.put(event)));
-
-        log('Successfully committed events for %s', this.entityType.name);
-
-        entity.newEvents = [];
-    }
-
-    private async commitSnapshots(
-        entity: TEntity,
-        options = { forceSnapshot: false },
-    ) {
-        if (
-            options.forceSnapshot ||
-            entity.version >= entity.snapshotVersion + this.snapshotFrequency
-        ) {
-            const snapshot = entity.snapshot();
-
-            log('Inserting snapshot for %s', this.entityType.name);
-
-            /**
-             * TODO - map to new snapshot put dynamodb operation
-             */
-            await this.snapshots.put(snapshot);
-
-            log('Successfully committed snapshot for %s', this.entityType.name);
-        }
-    }
-
-    private emitEvents(entity: TEntity) {
-        const eventsToEmit = entity.eventsToEmit;
-        entity.eventsToEmit = [];
-        eventsToEmit.forEach((event) => {
-            const args = Array.from(event);
-
-            // apply is necessary to allow outside control of event names
-            this.entityType.prototype.emit.apply(entity, args);
-        });
-    }
+      // apply is necessary to allow outside control of event names
+      this.entityType.prototype.emit.apply(entity, args);
+    });
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,184 +2,182 @@ import { Entity } from 'sourced';
 import { Repository } from '../src/index';
 
 const mockSavedTestEntityEvents = {
-    Items: [
-        {
-            method: 'init',
-            data: undefined,
-            timestamp: 1606703172298,
-            version: 1,
-        },
-        {
-            method: 'addOne',
-            data: undefined,
-            timestamp: 1606703172298,
-            version: 2,
-        },
-        {
-            method: 'addOne',
-            data: undefined,
-            timestamp: 1606703172298,
-            version: 3,
-        },
-    ],
+  Items: [
+    {
+      method: 'init',
+      data: undefined,
+      timestamp: 1606703172298,
+      version: 1,
+    },
+    {
+      method: 'addOne',
+      data: undefined,
+      timestamp: 1606703172298,
+      version: 2,
+    },
+    {
+      method: 'addOne',
+      data: undefined,
+      timestamp: 1606703172298,
+      version: 3,
+    },
+  ],
 };
 
 const ArcTablesDynamoMock = {
-    testentityevents: {
-        put: jest.fn().mockResolvedValue(undefined),
-        get: jest.fn().mockResolvedValue(mockSavedTestEntityEvents),
-        query: jest.fn().mockResolvedValue(mockSavedTestEntityEvents),
-    },
-    testentitysnapshots: {
-        put: jest.fn().mockResolvedValue(undefined),
-        query: jest.fn().mockResolvedValue({ Items: [] }),
-        get: jest.fn().mockResolvedValue({
-            _eventsCount: 0,
-            snapshotVersion: 3,
-            timestamp: 1606703254404,
-            version: 3,
-            total: 2,
-            id: 'test-id',
-        }),
-    },
+  testentityevents: {
+    put: jest.fn().mockResolvedValue(undefined),
+    get: jest.fn().mockResolvedValue(mockSavedTestEntityEvents),
+    query: jest.fn().mockResolvedValue(mockSavedTestEntityEvents),
+  },
+  testentitysnapshots: {
+    put: jest.fn().mockResolvedValue(undefined),
+    query: jest.fn().mockResolvedValue({ Items: [] }),
+    get: jest.fn().mockResolvedValue({
+      _eventsCount: 0,
+      snapshotVersion: 3,
+      timestamp: 1606703254404,
+      version: 3,
+      total: 2,
+      id: 'test-id',
+    }),
+  },
 };
 
 class TestEntity extends Entity {
-    id: string;
-    total: number;
+  id: string;
+  total: number;
 
-    constructor(snapshot?: any, events?: any) {
-        super();
+  constructor(snapshot?: any, events?: any) {
+    super();
 
-        this.total = 0;
-        this.id = '';
+    this.total = 0;
+    this.id = '';
 
-        this.rehydrate(snapshot, events);
-    }
+    this.rehydrate(snapshot, events);
+  }
 
-    init() {
-        this.id = 'test-id';
-        this.digest('init', {});
-    }
+  init() {
+    this.id = 'test-id';
+    this.digest('init', {});
+  }
 
-    addOne() {
-        this.total += 1;
-        this.digest('addOne', {});
-        this.enqueue('oneAdded');
-    }
+  addOne() {
+    this.total += 1;
+    this.digest('addOne', {});
+    this.enqueue('oneAdded');
+  }
 }
 
 describe('Repository tests', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-    it('should create a Repository', () => {
-        const repo = new Repository(TestEntity);
+  it('should create a Repository', () => {
+    const repo = new Repository(TestEntity, { dynamoTable: 'TestTable' });
 
-        expect(repo).toBeInstanceOf(Repository);
-    });
+    expect(repo).toBeInstanceOf(Repository);
+  });
 
-    it('should commit events and snapshot when forceSnapshot: true', async () => {
-        const testEntity = new TestEntity();
-        testEntity.init();
-        testEntity.addOne();
-        testEntity.addOne();
+  it('should commit events and snapshot when forceSnapshot: true', async () => {
+    const testEntity = new TestEntity();
+    testEntity.init();
+    testEntity.addOne();
+    testEntity.addOne();
 
-        const repo = new Repository(TestEntity);
+    const repo = new Repository(TestEntity, { dynamoTable: 'TestTable' });
 
-        const oneAdded = new Promise((resolve) =>
-            testEntity.once('oneAdded', resolve),
-        );
-
-        await repo.init();
-        await repo.commit(testEntity as any, { forceSnapshot: true });
-
-        await oneAdded;
-
-        expect(
-            ArcTablesDynamoMock.testentityevents.put.mock.calls[0][0].method,
-        ).toEqual('init');
-        expect(
-            ArcTablesDynamoMock.testentityevents.put.mock.calls[1][0].method,
-        ).toEqual('addOne');
-        expect(
-            ArcTablesDynamoMock.testentityevents.put.mock.calls[2][0].method,
-        ).toEqual('addOne');
-        expect(
-            ArcTablesDynamoMock.testentitysnapshots.put.mock.calls[0][0].total,
-        ).toEqual(2);
-    });
-
-    it('should only commit events when snapshot frequency is not met', async () => {
-        const testEntity = new TestEntity();
-        testEntity.init();
-        testEntity.addOne();
-        testEntity.addOne();
-
-        const repo = new Repository(TestEntity);
-
-        const oneAdded = new Promise((resolve) =>
-            testEntity.once('oneAdded', resolve),
-        );
-
-        await repo.init();
-        await repo.commit(testEntity);
-
-        await oneAdded;
-
-        expect(
-            ArcTablesDynamoMock.testentityevents.put.mock.calls[0][0].method,
-        ).toEqual('init');
-        expect(
-            ArcTablesDynamoMock.testentityevents.put.mock.calls[1][0].method,
-        ).toEqual('addOne');
-        expect(
-            ArcTablesDynamoMock.testentityevents.put.mock.calls[2][0].method,
-        ).toEqual('addOne');
-        expect(
-            ArcTablesDynamoMock.testentitysnapshots.put,
-        ).not.toHaveBeenCalled();
-    });
-
-    it('should fire enqueued events after successful commit', async () => {
-        const testEntity = new TestEntity();
-        const repo = new Repository(TestEntity);
-        await repo.init();
-
-        testEntity.init();
-        testEntity.addOne();
-        testEntity.addOne();
-
-        let emitted = 0;
-
-        testEntity.on('oneAdded', () => {
-            emitted++;
-        });
-
-        expect(testEntity.eventsToEmit.length).toBe(2);
-
-        await repo.commit(testEntity);
-
-        expect(emitted).toBe(2);
-    });
-
-    it('should be able to retrieve entity that only has events', async () => {
-        const repo = new Repository(TestEntity);
-        await repo.init();
-
-        const testEntity = await repo.get('test-id');
-
-        expect(testEntity.id).toBe('test-id');
-        expect(testEntity.total).toBe(2);
-    });
-    it.todo(
-        'should retrieve latest snapshot and events for entity and merge together into model',
+    const oneAdded = new Promise((resolve) =>
+      testEntity.once('oneAdded', resolve),
     );
-    it.todo('should throw error when commit fails for events');
-    it.todo('should throw when commit fails for events');
+
+    await repo.init();
+    await repo.commit(testEntity as any, { forceSnapshot: true });
+
+    await oneAdded;
+
+    expect(
+      ArcTablesDynamoMock.testentityevents.put.mock.calls[0][0].method,
+    ).toEqual('init');
+    expect(
+      ArcTablesDynamoMock.testentityevents.put.mock.calls[1][0].method,
+    ).toEqual('addOne');
+    expect(
+      ArcTablesDynamoMock.testentityevents.put.mock.calls[2][0].method,
+    ).toEqual('addOne');
+    expect(
+      ArcTablesDynamoMock.testentitysnapshots.put.mock.calls[0][0].total,
+    ).toEqual(2);
+  });
+
+  it('should only commit events when snapshot frequency is not met', async () => {
+    const testEntity = new TestEntity();
+    testEntity.init();
+    testEntity.addOne();
+    testEntity.addOne();
+
+    const repo = new Repository(TestEntity, { dynamoTable: 'TestTable' });
+
+    const oneAdded = new Promise((resolve) =>
+      testEntity.once('oneAdded', resolve),
+    );
+
+    await repo.init();
+    await repo.commit(testEntity);
+
+    await oneAdded;
+
+    expect(
+      ArcTablesDynamoMock.testentityevents.put.mock.calls[0][0].method,
+    ).toEqual('init');
+    expect(
+      ArcTablesDynamoMock.testentityevents.put.mock.calls[1][0].method,
+    ).toEqual('addOne');
+    expect(
+      ArcTablesDynamoMock.testentityevents.put.mock.calls[2][0].method,
+    ).toEqual('addOne');
+    expect(ArcTablesDynamoMock.testentitysnapshots.put).not.toHaveBeenCalled();
+  });
+
+  it('should fire enqueued events after successful commit', async () => {
+    const testEntity = new TestEntity();
+    const repo = new Repository(TestEntity, { dynamoTable: 'TestTable' });
+    await repo.init();
+
+    testEntity.init();
+    testEntity.addOne();
+    testEntity.addOne();
+
+    let emitted = 0;
+
+    testEntity.on('oneAdded', () => {
+      emitted++;
+    });
+
+    expect(testEntity.eventsToEmit.length).toBe(2);
+
+    await repo.commit(testEntity);
+
+    expect(emitted).toBe(2);
+  });
+
+  it('should be able to retrieve entity that only has events', async () => {
+    const repo = new Repository(TestEntity, { dynamoTable: 'TestTable' });
+    await repo.init();
+
+    const testEntity = await repo.get('test-id');
+
+    expect(testEntity.id).toBe('test-id');
+    expect(testEntity.total).toBe(2);
+  });
+  it.todo(
+    'should retrieve latest snapshot and events for entity and merge together into model',
+  );
+  it.todo('should throw error when commit fails for events');
+  it.todo('should throw when commit fails for events');
 });
