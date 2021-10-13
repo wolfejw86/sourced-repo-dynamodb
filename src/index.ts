191e2f614236527c92d868e9ddd27790acdac268
Jay Wolfe <wolfejw86@gmail.com>
2021-10-12 22:21:49 -0400

    finished "get" and "commit" methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { Entity } from 'sourced';
 import debug from 'debug';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBDocumentClient,
+  TransactWriteCommand,
+  TransactWriteCommandInput,
+  QueryCommand,
+} from '@aws-sdk/lib-dynamodb';
 
 const log = debug('sourced-repo-dynamodb');
 
@@ -9,12 +16,14 @@ interface TEntityType<TEntity extends Entity> {
 
 interface RepositoryOptions {
   dynamoTable: string;
+  awsRegion: string;
   snapshotFrequency: number;
   dynamoIndex?: string;
 }
 
 interface RepositoryInitOptions {
   dynamoTable: string;
+  awsRegion?: string;
   snapshotFrequency?: number;
   dynamoIndex?: string;
 }
@@ -40,6 +49,7 @@ export class Repository<TEntity extends Entity & { id: string }> {
   private snapshots: any;
   private options: RepositoryOptions;
   private entityName: string;
+  private client: DynamoDBDocumentClient;
 
   constructor(
     private entityType: TEntityType<TEntity>,
@@ -48,28 +58,80 @@ export class Repository<TEntity extends Entity & { id: string }> {
     this.options = Object.assign(
       {
         snapshotFrequency: 10,
+        awsRegion: 'us-east-1',
       },
       options,
     );
 
     this.snapshotFrequency = this.options.snapshotFrequency;
     this.entityName = entityType.name.toLowerCase();
-    this.eventPrefix = `${this.entityName}events`;
-    this.snapshotPrefix = `${this.entityName}snapshots`;
+    this.eventPrefix = `${this.entityName}events#`;
+    this.snapshotPrefix = `${this.entityName}snapshots#`;
+
+    this.client = DynamoDBDocumentClient.from(
+      new DynamoDBClient({
+        region: this.options.awsRegion,
+      }),
+      {
+        marshallOptions: {
+          // Whether to automatically convert empty strings, blobs, and sets to `null`.
+          convertEmptyValues: false, // false, by default.
+          // Whether to remove undefined values while marshalling.
+          removeUndefinedValues: true, // false, by default.
+          // Whether to convert typeof object to map attribute.
+          convertClassInstanceToMap: true, // false, by default.
+        },
+      },
+    );
   }
 
-  init() {
+  async init() {
     log('init');
   }
 
   async get(id: string) {
-    // get latest snapshot by id
-    // get latest events after latest snapshot by entity's id
+    const [snapshotResult, eventsResult] = await Promise.all([
+      this.client.send(
+        new QueryCommand({
+          TableName: this.options.dynamoTable,
+          KeyConditionExpression: '#pk = :pk AND begins_with(#sk, :sk)',
+          ExpressionAttributeNames: {
+            '#pk': 'PK',
+            '#sk': 'SK',
+          },
+          ExpressionAttributeValues: {
+            ':pk': `${this.entityName}#${id}`,
+            ':sk': this.snapshotPrefix,
+          },
+          ScanIndexForward: false,
+          Limit: 1,
+        }),
+      ),
+      this.client.send(
+        new QueryCommand({
+          TableName: this.options.dynamoTable,
+          KeyConditionExpression: '#pk = :pk AND begins_with(#sk, :sk)',
+          ExpressionAttributeNames: {
+            '#pk': 'PK',
+            '#sk': 'SK',
+          },
+          ExpressionAttributeValues: {
+            ':pk': `${this.entityName}#${id}`,
+            ':sk': this.eventPrefix,
+          },
+          ScanIndexForward: false,
+          Limit: this.snapshotFrequency,
+        }),
+      ),
+    ]);
 
-    log(id);
+    const { data: snapshot } = snapshotResult.Items?.[0] || {};
+    const events = (eventsResult.Items || [])
+      .map((event) => event.data)
+      .filter((event) => event.version > (snapshot?.version || 0));
 
     // merge snapshot and events with new entity
-    return new this.entityType();
+    return new this.entityType(snapshot, events);
   }
 
   async getAll(ids: string[]) {
@@ -84,18 +146,58 @@ export class Repository<TEntity extends Entity & { id: string }> {
 
   async commit(
     entity: TEntity,
-    options: { forceSnapshot: boolean; withTransaction?: boolean } = {
+    options: { forceSnapshot: boolean } = {
       forceSnapshot: false,
-      withTransaction: true,
     },
   ) {
-    // write events
-    // write snapshot
-    // throw on error
+    if (!entity.id) {
+      new Error(
+        `Cannot commit an entity of type [${this.entityType.name}] without an [id] property.`,
+      );
+    }
 
-    log(options);
+    const writeItems: TransactWriteCommandInput['TransactItems'] =
+      entity.newEvents.map((event) => {
+        return {
+          Put: {
+            TableName: this.options.dynamoTable,
+            Item: {
+              PK: `${this.entityName}#${entity.id}`,
+              SK: `${this.eventPrefix}${event.version}`,
+              data: event,
+            },
+          },
+        };
+      });
 
-    return;
+    if (
+      options.forceSnapshot ||
+      entity.version >= entity.snapshotVersion + this.snapshotFrequency
+    ) {
+      const snapshot = entity.snapshot();
+      log('writing snapshot', snapshot);
+
+      writeItems.push({
+        Put: {
+          TableName: this.options.dynamoTable,
+          Item: {
+            PK: `${this.entityName}#${entity.id}`,
+            SK: `${this.snapshotPrefix}${entity.snapshotVersion}`,
+            data: entity.snapshot(),
+          },
+        },
+      });
+    }
+
+    const writeTxn = new TransactWriteCommand({
+      TransactItems: writeItems,
+    });
+
+    const res = await this.client.send(writeTxn);
+
+    entity.newEvents = [];
+
+    return res;
   }
 
   async commitAll(
